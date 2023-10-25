a51df16f3c31c99f1edd2a17a727726d5ff1ddba
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-10-25 12:59:46 +0600

    addTask

--- script/script.js
+++ script/script.js
@@ -5,7 +5,6 @@ let taskList = document.getElementById("task-list");
 let clearTaskBtn = document.getElementById("clear-task-btn");
 
 addTaskForm.addEventListener("submit", addTask);
-taskList.addEventListener("click", removeTask);
 
 function addTask(e) {
   if (taskInput.value === "") alert("Add a Task");
@@ -19,10 +18,4 @@ function addTask(e) {
     taskInput.value = "";
   }
   e.preventDefault();
-}
-
-function removeTask(e) {
-  if (e.target.tagName === "BUTTON") {
-    if (confirm("Are you sure?")) e.target.parentElement.remove();
-  }
 }
